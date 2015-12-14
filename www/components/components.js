1d92331fdc3e550cdc9caf27f92c41748a79c6ee
Maxime Chalendar <maximech86@gmail.com>
2015-12-14 14:01:20 +0200

    Initial Commit

---
+++ www/components/components.js
@@ -0,0 +1,51 @@
+angular.module('components', ['ionic'])
+
+  .directive('preImg', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {
+        ratio:'@',
+        helperClass: '@'
+      },
+      controller: function($scope) {
+        $scope.loaded = false;
+
+        this.hideSpinner = function(){
+          // Think i have to use apply because this function is not called from this controller ($scope)
+          $scope.$apply(function () {
+            $scope.loaded = true;
+          });
+        };
+      },
+      templateUrl: 'components/pre-img.html'
+    };
+  })
+
+  .directive('spinnerOnLoad', function() {
+    return {
+      restrict: 'A',
+      require: '^preImg',
+      scope: {
+        ngSrc: '@'
+      },
+      link: function(scope, element, attr, preImgController) {
+        element.on('load', function() {
+          preImgController.hideSpinner();
+        });
+      }
+    };
+  })
+
+
+  .directive('backImg', function(){
+    return function(scope, element, attrs){
+      attrs.$observe('backImg', function(value) {
+        element.css({
+          'background-image': "url('" + value +"')"
+        });
+      });
+    };
+  })
+
+;
